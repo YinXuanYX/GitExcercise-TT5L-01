a12ed086641d94a2c6c03279543e6d51dd5cbf24
Yin Xuan <tanyinxuanxuan@gmail.com>
2024-04-25 00:15:24 +0800

    added main to my local

---
+++ main.py
@@ -0,0 +1,50 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QGridLayout, QVBoxLayout, QHBoxLayout, QWidget
+from PyQt6.QtGui import QIcon
+from PyQt6.QtCore import Qt
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setMinimumSize(720,1280)
+        self.setWindowTitle("Mobile Legends Ultimate Guide")
+        self.setWindowIcon(QIcon("Logo_Mobile_Legends-_Bang_Bang.jpg"))
+
+        parentLayout = QVBoxLayout()
+        buttonLayout = QHBoxLayout()
+
+        self.button1 = QPushButton("Heroes")
+        self.button2 = QPushButton("Skins")
+        self.button3 = QPushButton("Events and recharge")
+        self.button4 = QPushButton("Rank system and line ups")
+
+        buttonLayout.addWidget(self.button1)
+        buttonLayout.addSpacing(50)
+        buttonLayout.addWidget(self.button2)
+        buttonLayout.addSpacing(50)
+        buttonLayout.addWidget(self.button3)
+        buttonLayout.addSpacing(50)
+        buttonLayout.addWidget(self.button4)
+
+        self.label = QLabel("Welcome to the best guides app for Mobile Legends Bang Bang", alignment=Qt.AlignmentFlag.AlignCenter)
+        font = self.font()
+        font.setPointSize(24)
+        font.setBold(True)
+        self.label.setFont(font)
+        parentLayout.addLayout(buttonLayout)
+
+        parentLayout.addWidget(self.label)
+
+        centerWidget= QWidget()
+        centerWidget.setLayout(parentLayout)
+        self.setCentralWidget(centerWidget)
+
+app = QApplication([])
+
+window = Window()
+
+
+window.show()
+app.exec()
+
+
