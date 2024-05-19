a21cc0b4bb7740b17f4783d55370777978d26780
Yin Xuan <tanyinxuanxuan@gmail.com>
2024-05-20 00:54:54 +0800

    Completed the database for our items

--- items.py
+++ items.py
@@ -2,6 +2,7 @@ from PySide6.QtCore import Qt
 from ui_items import Ui_MainWindow
 from PySide6.QtGui import QIcon
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
+from items_firstdb import *
 
 class MySideBar(QMainWindow, Ui_MainWindow):
     def __init__(self):
