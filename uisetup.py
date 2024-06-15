196b549768335d2137a8a809561d5493db9eb796
Prabu Natar <prabunatar@gmail.com>
2024-06-15 21:30:37 +0800

    Error solving

--- uisetup.py
+++ uisetup.py
@@ -8,11 +8,18 @@ from functools import partial
 
 
 
-class heroinfo(QMainWindow,Ui_MainWindow):
-    def __init__(self):
+class heroinfo(QMainWindow,Ui_MainWindow): #The class is the generated mainwindow and the compiling of the ui file data that will be runned on the window 
+    def __init__(self): #intializes every code with the parameter self to integrate into the main window.
         super().__init__()
-        self.setupUi(self)
-        self.setWindowTitle("MLBB HERO GUIDE")
+        self.setupUi(self) #Sets up the ui to function with the class
+        self.setWindowTitle("MLBB ULTIMATE GUIDE") #Sets the name of the window
+
+        #set landing page to open first
+        self.stackedWidget.setCurrentIndex(0)
+
+        #Signals and slots for all the buttons to respond according to the functions defined below.
+        #Addtional note, partial allows the developer to make the signals and slot more dynamic by allowing the number of Id in database to be selected when the saberpg function is called.
+        #use partial to avoid the extra argument problem so each button correctly passes item ID to each buttons of heroes.
         self.allhero.clicked.connect(self.allpg)
         self.assa.clicked.connect(self.assasinpg)
         self.tank.clicked.connect(self.tenkpg)
@@ -82,32 +89,35 @@ class heroinfo(QMainWindow,Ui_MainWindow):
         self.Nana_2.clicked.connect(partial(self.saberpg, 30))
       
     def allpg(self):
-        self.stackedWidget.setCurrentIndex(0)
+        self.stackedWidget.setCurrentIndex(0) #Change the page index number to reflect the page created for buttons for all heroes
         
     def assasinpg(self):
-        self.stackedWidget.setCurrentIndex(1)
+        self.stackedWidget.setCurrentIndex(1) #Change the page index number to reflect the page created for buttons for assasin heroes
 
     def tenkpg(self):
-        self.stackedWidget.setCurrentIndex(2)
+        self.stackedWidget.setCurrentIndex(2) #Change the page index number to reflect the page created for buttons for tank heroes heroes
 
     def magpg(self):
-        self.stackedWidget.setCurrentIndex(4)
+        self.stackedWidget.setCurrentIndex(4) #Change the page index number to reflect the page created for buttons for mage heroes
 
     def fightpg(self):
-        self.stackedWidget.setCurrentIndex(3)
+        self.stackedWidget.setCurrentIndex(3) #Change the page index number to reflect the page created for buttons for fighter heroes
 
     def marskmpg(self):
-        self.stackedWidget.setCurrentIndex(5)
+        self.stackedWidget.setCurrentIndex(5) #Change the page index number to reflect the page created for buttons for marskman heroes
 
     def suppg(self):
-        self.stackedWidget.setCurrentIndex(6)
-
+        self.stackedWidget.setCurrentIndex(6) #Change the page index number to reflect the page created for buttons for support heroes
+    
+    #*args allows the thing to accept any number of additional arguments, solve the issue of receiving more arguments than expected from the clicked signal. Basically for the double digit.
     def saberpg(self, item_id, *args):
-        self.stackedWidget.setCurrentIndex(7)
-        conn = sqlite3.connect('heroes.db')
-        cursor = conn.cursor()
+        self.stackedWidget.setCurrentIndex(7) #Change the page index number to reflect the page created for the data of heroes to be displayed
+        conn = sqlite3.connect('heroes.db') #creates a connection to the database file
+        cursor = conn.cursor() #creates a curson object that will help select things from the database
+        # Ensure the item_id is passed as a tuple
         cursor.execute('SELECT Passive_name, Passive, Skill_1_name, Skill_1, Skill_2_name, Skill_2, Skill_3_name, Skill_3, Ultimate_name, Ultimate, Heroimage, passive_image, skill1_image, skill2_image, ultimate_image FROM heroes WHERE Id = ?', (item_id,))
-        result = cursor.fetchone()
+        #The code above will query the data from database.
+        result = cursor.fetchone() #The draw the data from the database to be used as a variable
 
         if result:
             (passive_name, passive, 
@@ -116,8 +126,10 @@ class heroinfo(QMainWindow,Ui_MainWindow):
              skill_3_name, skill_3, 
              ultimate_name, ultimate, 
              Heroimage, passive_image, 
-             skill1_image, skill2_image, ultimate_image) = result
+             skill1_image, skill2_image, ultimate_image) = result #This turns the variable into multiple variables that contains the data according to the selection 
             
+            #The list of code down here is all the labels that will undergo modification which is to reflect the text in the label to the one in the database
+            #There is also modification of images which are done using the class Qpixmap which allows the developer to set images in the place of the label according to the prefix of the resource file.
             self.Passive_name_label.setText(f"{passive_name}")
             self.passivedesc.setText(f" {passive}")
             self.skill1_name.setText(f" {skill_1_name}")
@@ -136,6 +148,4 @@ class heroinfo(QMainWindow,Ui_MainWindow):
         else:
             self.Passive_name_label.setText("No passive name found.")
 
-        conn.close()
-        
- 
+        conn.close() #This will close the connection
